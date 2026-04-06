4ebe525ff79a240048439cf15b99138cfb5a3aed
Billie Fauzan <kuliahkuliahbillie@gmail.com>
2026-04-06 16:18:37 +0700

    menambahkan iostream & string library

--- struct.cpp
+++ struct.cpp
@@ -2,35 +2,26 @@
 #include <string>
 using namespace std;
 
-struct detailAlamat
-{
-    string desa;
-    string kota;
-};
-
 struct orang
 {
     string nama;
-    detailAlamat alamat;
+    string alamat;
     int umur;
 };
 
 int main() {
     orang mhs;
     cout << "Nama : " ;
-    getline(cin, mhs.nama);
-    cout << "alamat desa : " ;
-    getline(cin, mhs.alamat.desa);
-    cout << "alamat kota : " ;
-    getline(cin, mhs.alamat.kota);
+    cin >> mhs.nama;
+    cout << "alamat : " ;
+    cin >> mhs.alamat;
     cout << "umur : " ;
     cin >> mhs.umur;
     cout << endl;
 
     // Tampilan
     cout << " Nama : " << mhs.nama << endl;
-    cout << " Nama : " << mhs.alamat.desa << endl;
-    cout << " Nama : " << mhs.alamat.kota << endl;
+    cout << " Nama : " << mhs.alamat << endl;
     cout << " Nama : " << mhs.umur << endl;
 
 }
