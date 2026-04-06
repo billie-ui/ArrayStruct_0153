6d62964838c266fb4382d133060ba879f153deeb
Billie Fauzan <kuliahkuliahbillie@gmail.com>
2026-04-06 15:42:18 +0700

    menampilkan data array satu per satu

--- Array.cpp
+++ Array.cpp
@@ -25,6 +25,14 @@ int main () {
     cout << "data pada index 4 =";
     cin >> kamu[4];
     cout << endl;
+        // menampilkan satu persatu
+    cout << "Data Pertama = " << kamu[0] << endl;
+    cout << "Data Kedua = " << kamu[1] << endl;
+    cout << "Data Ketiga = " << kamu[2] << endl;
+    cout << "Data Keempat = " << kamu[3] << endl;
+    cout << "Data Kelima = " << kamu[4] << endl;
+    cout << endl;
+
 
 
 }
